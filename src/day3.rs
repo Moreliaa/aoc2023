e4c317984b8876d3425aaf1bf32930a5cc496b59
Moreliaa <eth_er@gmx.de>
2023-11-30 19:01:39 +0100

    add script files

---
+++ src/day3.rs
@@ -0,0 +1,22 @@
+pub fn run(input: String) {
+    println!("Day1 Pt1: {}", pt1(&input));
+    println!("Day1 Pt2: {}", pt2(&input));
+}
+
+fn pt1(input: &String) -> i32 {
+    0
+}
+
+fn pt2(input: &String) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[test]
+    fn test() {
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use aoc_lib::input_reader;
+use std::env;
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut run_all = false;
+
+    if args.len() > 1 && (args[1] == "a" || args[1] == "all") {
+        run_all = true;
+    }
+    day1::run(input("1"));
+    if run_all {
+        //day2::run(input("2"));
+        //day3::run(input("3"));
+        //day4::run();
+        //day5::run(input("5"));
+        //day6::run(input("6"));
+        //day7::run(input("7"));
+        //day8::run(input("8"));
+        //day9::run(input("9"));
+        //day10::run();
+        //day11::run();
+        //day12::run(input("12"));
+        //day13::run(input("13"));
+        //day14::run(input("14"));
+        //day15::run(input("15"));
+        //day16::run(input("16"));
+        //day17::run(input("17"));
+        //day18::run(input("18"));
+        //day19::run(input("19"));
+        //day20::run();
+        //day21::run();
+        //day22::run();
+        //day23::run(input("23"));
+        //day24::run(input("24"));
+        //day25::run();
+    }
+}
+
+fn input(day: &str) -> String {
+    input_reader::get_input("2023", day, "./cookie.txt")
 }
