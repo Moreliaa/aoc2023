433908ab7038e5af978cd319cfbeaf1488807809
Moreliaa <eth_er@gmx.de>
2023-12-01 09:43:46 +0100

    day1

--- src/day1.rs
+++ src/day1.rs
@@ -3,20 +3,61 @@ pub fn run(input: String) {
     println!("Day1 Pt2: {}", pt2(&input));
 }
 
-fn pt1(input: &String) -> i32 {
-    0
-}
-
-fn pt2(input: &String) -> i32 {
-    0
-}
-
-#[cfg(test)]
-mod tests {
-    #[allow(unused_imports)]
-    use super::*;
-    #[test]
-    fn test() {
-        
+fn pt1(input: &String) -> u32 {
+    let mut sum = 0;
+    for l in input.lines() {
+        let mut first = None;
+        let mut last = None;
+        for c in l.chars() {
+            match c {
+                '0'..='9' => {
+                    if first == None {
+                        first = c.to_digit(10);
+                        
+                    }
+                    last = c.to_digit(10);
+                },
+                _ => () 
+            }
+        }
+        sum += first.unwrap() * 10 + last.unwrap();
     }
+    sum
+}
+
+fn pt2(input: &String) -> u32 {
+    let mut sum = 0;
+    let numbers = ["zero", "one","two","three","four","five","six","seven","eight","nine"];
+    for l in input.lines() {
+        let mut first = None;
+        let mut last = None;
+        println!("{}", l);
+        for (i, c) in l.chars().enumerate() {
+            match c {
+                '0'..='9' => {
+                    if first == None {
+                        first = c.to_digit(10);
+                        
+                    }
+                    last = c.to_digit(10);
+                },
+                _ => () 
+            }
+
+            for i_n in 0..numbers.len() {
+                let n = numbers[i_n];
+                if i >= n.len() - 1 {
+                    let i_s = i + 1 - n.len();
+                    if &l[i_s..=i] == n {
+                        if first == None {
+                            first = Some(i_n as u32);
+                        }
+                        last = Some(i_n as u32);
+                    }
+                }
+            }
+        }
+        sum += first.unwrap() * 10 + last.unwrap();
+    }
+    sum
 }
