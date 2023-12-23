64ba54e31f9db6cc42dd88f590e932c74cc5f8c5
Moreliaa <eth_er@gmx.de>
2023-12-23 19:03:31 +0100

    day23 pt2

--- src/day23.rs
+++ src/day23.rs
@@ -2,9 +2,10 @@ use aoc_lib::map2d::Map2D;
 use std::collections::{HashMap, HashSet};
 
 pub fn run(input: String) {
-    let map = Map2D::from_string(input);
+    let map = Map2D::from_string(input.clone());
     println!("Day23 Pt1: {}", pt1(&map));
-    //println!("Day23 Pt2: {}", pt2(&input));
+    let mut map = Map2D::from_string(input);
+    println!("Day23 Pt2: {}", pt2(&mut map));
 }
 
 #[derive(Clone)]
@@ -13,6 +14,9 @@ struct Path {
     x: i32,
     y: i32,
     steps: i32,
+    last_x: i32, // pt2
+    last_y: i32,
+    last_steps: i32,
 }
 
 fn pt1(map: &Map2D<char>) -> i32 {
@@ -38,6 +42,9 @@ fn pt1(map: &Map2D<char>) -> i32 {
         x: start_x,
         y: start_y,
         steps: 0,
+        last_x: 0,
+        last_y: 0,
+        last_steps: 0,
     }];
 
     let dir = [(0, -1), (0, 1), (-1, 0), (1, 0)];
@@ -114,8 +121,163 @@ fn pt1(map: &Map2D<char>) -> i32 {
     steps.into_iter().fold(0, |acc, v| acc.max(v))
 }
 
-fn pt2(input: &String) -> i32 {
-    0
+fn pt2(map: &mut Map2D<char>) -> i32 {
+    let (mut start_x, start_y) = (0, 0);
+    for x in 0..map.width() {
+        if *map.get(x, start_y).unwrap() != '#' {
+            start_x = x;
+            break;
+        }
+    }
+
+    let (mut end_x, end_y) = (0, map.height() - 1);
+    for x in 0..map.width() {
+        if *map.get(x, end_y).unwrap() != '#' {
+            end_x = x;
+            break;
+        }
+    }
+
+    let mut paths = vec![Path {
+        seen: HashSet::new(),
+        x: start_x,
+        y: start_y,
+        steps: 0,
+        last_x: start_x,
+        last_y: start_y,
+        last_steps: 0,
+    }];
+    let mut graph: HashMap<(i32, i32), HashMap<(i32, i32), i32>> = HashMap::new();
+    graph.insert((start_x, start_y), HashMap::new());
+    let mut seen_graph: HashSet<(i32, i32)> = HashSet::new();
+
+    let dir = [(0, -1), (0, 1), (-1, 0), (1, 0)];
+    while let Some(mut path) = paths.pop() {
+        if path.x == end_x && path.y == end_y {
+            if !graph.contains_key(&(end_x, end_y)) {
+                graph.insert((end_x, end_y), HashMap::new());
+            }
+            let dist = path.steps - path.last_steps;
+            graph.entry((end_x, end_y)).and_modify(|e| {
+                e.insert((path.last_x, path.last_y), dist);
+            });
+            graph.entry((path.last_x, path.last_y)).and_modify(|e| {
+                e.insert((end_x, end_y), dist);
+            });
+            continue;
+        }
+        let mut possible_steps = vec![];
+        for d in dir.iter() {
+            let x1 = path.x + d.0;
+            let y1 = path.y + d.1;
+            if path.seen.contains(&(x1, y1)) {
+                continue;
+            }
+
+            if let Some(tile) = map.get(x1, y1) {
+                match *tile {
+                    '#' => continue,
+                    _ => (),
+                }
+            } else {
+                continue;
+            }
+
+            possible_steps.push((x1, y1));
+        }
+
+        let poss_len = possible_steps.len();
+        /*if (path.x != start_x || path.y != start_y) && graph.contains_key(&(path.x, path.y)) {
+            continue;
+        }*/
+
+        if poss_len > 1 {
+            if !graph.contains_key(&(path.x, path.y)) {
+                graph.insert((path.x, path.y), HashMap::new());
+            }
+
+            let dist = path.steps - path.last_steps;
+            graph.entry((path.x, path.y)).and_modify(|e| {
+                e.insert((path.last_x, path.last_y), dist);
+            });
+            graph.entry((path.last_x, path.last_y)).and_modify(|e| {
+                e.insert((path.x, path.y), dist);
+            });
+        }
+
+        if seen_graph.contains(&(path.x, path.y)) {
+            continue;
+        }
+        if poss_len > 1 {
+            seen_graph.insert((path.x, path.y));
+        }
+
+        for (idx, (x1, y1)) in possible_steps.into_iter().enumerate() {
+            /*if idx == poss_len - 1 {
+                path.seen.insert((path.x, path.y));
+                if poss_len > 1 {
+                    path.last_x = path.x;
+                    path.last_y = path.y;
+                    path.last_steps = path.steps;
+                }
+                path.x = x1;
+                path.y = y1;
+                path.steps += 1;
+                paths.push(path);
+                break;
+            //} else {*/
+            let mut new_path = path.clone();
+            new_path.seen.insert((path.x, path.y));
+            if poss_len > 1 {
+                new_path.last_x = path.x;
+                new_path.last_y = path.y;
+                new_path.last_steps = path.steps;
+            }
+            new_path.x = x1;
+            new_path.y = y1;
+            new_path.steps += 1;
+            paths.push(new_path);
+            //}
+        }
+    }
+
+    // find paths
+    let mut steps: Vec<i32> = vec![];
+    let mut paths = vec![Path {
+        seen: HashSet::new(),
+        x: start_x,
+        y: start_y,
+        steps: 0,
+        last_x: 0,
+        last_y: 0,
+        last_steps: 0,
+    }];
+
+    for coords in graph.keys() {
+        map.set(coords.0, coords.1, 'O');
+    }
+    map.print();
+
+    while let Some(path) = paths.pop() {
+        if path.x == end_x && path.y == end_y {
+            steps.push(path.steps);
+            continue;
+        }
+        // possible steps
+        let info = graph.get(&(path.x, path.y)).unwrap();
+        for (coords, steps) in info.iter() {
+            if path.seen.contains(&(coords.0, coords.1)) {
+                continue;
+            }
+            let mut new_path = path.clone();
+            new_path.seen.insert((path.x, path.y));
+            new_path.x = coords.0;
+            new_path.y = coords.1;
+            new_path.steps += steps;
+            paths.push(new_path);
+        }
+    }
+    steps.into_iter().fold(0, |acc, v| acc.max(v))
 }
 
 #[cfg(test)]
@@ -148,7 +310,8 @@ mod tests {
 #.....###...###...#...#
 #####################.#"
             .to_string();
-        let map = Map2D::from_string(input);
+        let mut map = Map2D::from_string(input);
         assert_eq!(pt1(&map), 94);
+        assert_eq!(pt2(&mut map), 154);
     }
 }
